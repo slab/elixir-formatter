1421a9135dc681924542b716673aa05591fe89b7
Chengyin Liu <chengyin@slab.com>
2017-11-22 15:58:49 -0800

    add favicons

--- lib/elixir_formatter_web/endpoint.ex
+++ lib/elixir_formatter_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule ElixirFormatterWeb.Endpoint do
     at: "/",
     from: :elixir_formatter,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico manifest.json robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
