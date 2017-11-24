40e86329d9be91f651abefd37f3e56d72ae008d8
Chengyin Liu <chengyin@slab.com>
2017-11-24 14:34:53 -0800

    add google analytics

--- config/prod.exs
+++ config/prod.exs
@@ -21,6 +21,9 @@ config :elixir_formatter, ElixirFormatterWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Google Analytics
+config :elixir_formatter, :ga, id: "UA-84928209-3"
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
